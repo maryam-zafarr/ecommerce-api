9c202a4c6320721d68d52e983790edf7baa677bb
Maryam <maryam.zafar@devsinc.com>
2022-11-01 22:57:10 +0500

    Change authorization for get requests

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ require("./auth/auth");
 const authRoute = require("./routes/auth");
 const userRoute = require("./routes/user");
 const productRoute = require("./routes/product");
+const cartRoute = require("./routes/cart");
 
 const app = express();
 
@@ -25,6 +26,7 @@ app.use(express.json());
 app.use("/api/auth", authRoute);
 app.use("/api/users", userRoute);
 app.use("/api/products", productRoute);
+app.use("/api/cart", cartRoute);
 
 app.use((err, req, res, next) => {
   res.status(err.status || 500);
